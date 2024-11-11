51c189c2e5fc025124ee1d5b94793286354f0d7e
Aiden <72966716+aidenbok203@users.noreply.github.com>
2024-11-11 22:44:53 +0000

    Add dataclass

--- Library.py
+++ Library.py
@@ -1,11 +1,12 @@
 from datetime import datetime
+from dataclasses import dataclass
 
+@dataclass
 class Book:
-    def __init__(self, id, title, author, availability = True):
-        self.id = id
-        self.title = title
-        self.author = author
-        self.availability = availability
+    id: int
+    title: str
+    author: str
+    availability: bool = True
 
     def __str__(self):
         return f"ID: {self.id}, Title: {self.title}, Author: {self.author}, Available: {self.availability}"
